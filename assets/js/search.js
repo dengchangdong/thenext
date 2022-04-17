40c83a8fa2c270148b5c2885ba1faaa86bcf611a
dengchangdong <dengchangdong@gmail.com>
2022-04-17 18:34:58 +0800

    Site updated: 2022-04-17 18:34:57

--- assets/js/search.js
+++ assets/js/search.js
@@ -19,104 +19,119 @@
 // 
 
 var searchFunc = function(path, search_id, content_id) {
-    "use strict";
+    'use strict';
     $.ajax({
         url: path,
         dataType: "xml",
         success: function( xmlResponse ) {
             // get the contents from search data
-            var datas = $("entry", xmlResponse).map(function() {
+            var datas = $( "entry", xmlResponse ).map(function() {
                 return {
-                    title: $("title", this).text(),
-                    content: $("content", this).text(),
-                    url: $("url", this).text()
+                    title: $( "title", this ).text(),
+                    content: $("content",this).text(),
+                    url: $( "url" , this).text()
                 };
             }).get();
+
             var $input = document.getElementById(search_id);
-            if (!$input) return;
+			if (!$input) return;
             var $resultContent = document.getElementById(content_id);
-            $input.addEventListener("input", function(){
-                var str="<ul class=\"result-list\">";                
-                var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
+            if ($("#local-search-input").length > 0) {
+                $input.addEventListener('input', function () {
+                var str="<ul class=\"result-list\">";   
+                    var keywords = this.value.trim().toLowerCase().split(/[\s\-]+/);
                     $resultContent.innerHTML = "<p class=\"result-icon\"><i class=\"icon-search genericons\"></i></p>";
-                if (this.value.trim().length <= 0) {
-                    $resultContent.classList.add("no-result");
-                    return;
-                }
-                // perform local searching
-                var numOfPostFound = 0; // keeping track of # of result
-                datas.forEach(function(data) {
-                    var isMatch = true;
-                    var content_index = [];
-                    var data_title = data.title.trim().toLowerCase();
-                    var data_content = data.content.trim().replace(/<[^>]+>/g,"").toLowerCase();
-                    var data_url = data.url;
-                    var index_title = -1;
-                    var index_content = -1;
-                    var first_occur = -1;
-                    // only match artiles with not empty titles and contents
-                    if(data_title != '' && data_content != "") {
-                        keywords.forEach(function(keyword, i) {
-                            index_title = data_title.indexOf(keyword);
-                            index_content = data_content.indexOf(keyword);
-                            if( index_title < 0 && index_content < 0 ){
-                                isMatch = false;
-                            } else {
-                                if (index_content < 0) {
-                                    index_content = 0;
-                                }
-                                if (i == 0) {
-                                    first_occur = index_content;
-                                }
-                            }
-                        });
+                    if (this.value.trim().length <= 0) {
+                        $resultContent.classList.add("no-result");
+                        return;
                     }
-                    // show search results
-                    if (isMatch) {
-                        $resultContent.classList.remove("no-result");
-                        numOfPostFound += 1; // keeping track of # of results
-                        str += "<li><a href='"+ data_url +"'>"+ data_title +"</a>";
-                        var content = data.content.trim().replace(/<[^>]+>/g,"");
-                        if (first_occur >= 0) {
-                            // cut out 100 characters
-                            var start = first_occur - 10;
-                            var end = first_occur + 20;
-                            if(start < 0){
-                                start = 0;
-                            }
-                            if(start == 0){
-                                end = 20;
-                            }
-                            if(end > content.length){
-                                end = content.length;
-                            }
-                            var match_content = content.substr(start, end); 
-                            // highlight all keywords
-                            keywords.forEach(function(keyword){
-                                var regS = new RegExp(keyword, "gi");
-                                match_content = match_content.replace(regS, "<em>"+keyword+"</em>");
-                            });
-                            
-                            str += "<p>" + match_content +"...</p>"
+                    // perform local searching
+                    var numOfPostFound = 0; // keeping track of # of result
+                    datas.forEach(function (data) {
+                        var isMatch = true;
+                        var content_index = [];
+                        if (!data.title || data.title.trim() === '') {
+                            data.title = "无标题";
                         }
-                        str += "</li>";
-                    }
-                });
-                str += "</ul>";
-                // attaching a summary of searching result
-                if (numOfPostFound > 0) {
-                    if (numOfPostFound > 1) {
-                        summary = "共找到 " + numOfPostFound + " 条记录";
+                        var data_title = data.title.trim().toLowerCase();
+                        var data_content = data.content.trim().replace(/<[^>]+>/g, "").toLowerCase();
+                        var data_url = data.url;
+                        var index_title = -1;
+                        var index_content = -1;
+                        var first_occur = -1;
+                        // only match artiles with not empty contents
+                        if (data_content !== '') {
+                            keywords.forEach(function (keyword, i) {
+                                index_title = data_title.indexOf(keyword);
+                                index_content = data_content.indexOf(keyword);
+
+                                if (index_title < 0 && index_content < 0) {
+                                    isMatch = false;
+                                } else {
+                                    if (index_content < 0) {
+                                        index_content = 0;
+                                    }
+                                    if (i == 0) {
+                                        first_occur = index_content;
+                                    }
+                                    // content_index.push({index_content:index_content, keyword_len:keyword_len});
+                                }
+                            });
+                        } else {
+                            isMatch = false;
+                        }
+                        // show search results
+                        if (isMatch) {
+                            $resultContent.classList.remove("no-result");
+                            numOfPostFound += 1; // keeping track of # of results
+                            str += "<li><a href='"+ data_url +"'>"+ data_title +"</a>";
+                            var content = data.content.trim().replace(/<[^>]+>/g, "");
+                            if (first_occur >= 0) {
+                                // cut out 100 characters
+                                var start = first_occur - 20;
+                                var end = first_occur + 60;
+
+                                if (start < 0) {
+                                    start = 0;
+                                }
+
+                                if (start == 0) {
+                                    end = 80;
+                                }
+
+                                if (end > content.length) {
+                                    end = content.length;
+                                }
+
+                                var match_content = content.substring(start, end);
+
+                                // highlight all keywords
+                                keywords.forEach(function (keyword) {
+                                    var regS = new RegExp(keyword, "gi");
+                                    match_content = match_content.replace(regS, "<em>" + keyword + "</em>");
+                                });
+
+                                str += "<p>" + match_content + "...</p>"
+                            }
+                            str += "</li>";
+                        }
+                    });
+                    str += "</ul>";
+                    // attaching a summary of searching result
+                    if (numOfPostFound > 0) {
+                        if (numOfPostFound > 1) {
+                            summary = "共找到 " + numOfPostFound + " 条记录";
+                        } else {
+                            summary = "共找到 " + numOfPostFound + " 条记录"; // Only one
+                        }
+                        var summary = "<p class=\"search-stats\">" + summary + "</p>";
                     } else {
-                        summary = "共找到 " + numOfPostFound + " 条记录"; // Only one
+                        $resultContent.classList.add("no-result");
+                        summary = "<p class=\"result-icon\">没有找到内容</p>";
                     }
-                    var summary = "<p class=\"search-stats\">" + summary + "</p>";
-                } else {
-                    $resultContent.classList.add("no-result");
-                    summary = "<p class=\"result-icon\">没有找到内容</p>";
-                }
-                $resultContent.innerHTML = summary + str;
-            });
+                    $resultContent.innerHTML = summary + str;
+                });
+            }
         }
     });
 }
